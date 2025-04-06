712f7c5d8f57425068f3833e2adab7c741859802
andelwebsolutions <info@andelwebsolutions.nl>
2025-04-06 17:56:04 +0200

    chore: update primary color and remove dark mode logic

---
+++ resources/js/pages/coming-soon.tsx
@@ -0,0 +1,13 @@
+import { Head } from '@inertiajs/react';
+
+export default function ComingSoon() {
+    return (
+        <>
+            <Head title="Coming Soon">
+                <link rel="preconnect" href="https://fonts.bunny.net" />
+                <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+            </Head>
+            <div className="bg-primary h-screen w-screen"></div>
+        </>
+    );
+}
