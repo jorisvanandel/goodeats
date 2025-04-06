bf18095d38eae8cfa1f28d814c41fa2472c78190
andelwebsolutions <info@andelwebsolutions.nl>
2025-04-06 18:11:15 +0200

    feat: add coming soon screen

--- resources/js/pages/coming-soon.tsx
+++ resources/js/pages/coming-soon.tsx
@@ -3,11 +3,10 @@ import { Head } from '@inertiajs/react';
 export default function ComingSoon() {
     return (
         <>
-            <Head title="Coming Soon">
-                <link rel="preconnect" href="https://fonts.bunny.net" />
-                <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-            </Head>
-            <div className="bg-primary h-screen w-screen"></div>
+            <Head title="Coming Soon"/>
+            <div className="bg-primary h-screen w-screen flex justify-center pt-70">
+                <h1 className="text-3xl uppercase font-medium text-secondary">Coming Soon</h1>
+            </div>
         </>
     );
 }
