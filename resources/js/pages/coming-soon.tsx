c475cbf9aab6842d1880f3cb3330e31ec964a6b0
andelwebsolutions <info@andelwebsolutions.nl>
2025-04-06 18:22:16 +0200

    fix: use dynamic view height

--- resources/js/pages/coming-soon.tsx
+++ resources/js/pages/coming-soon.tsx
@@ -4,7 +4,7 @@ export default function ComingSoon() {
     return (
         <>
             <Head title="Coming Soon"/>
-            <div className="bg-primary h-screen w-screen flex justify-center pt-[33vh]">
+            <div className="bg-primary h-dvh w-screen flex justify-center pt-[33vh]">
                 <h1 className="text-3xl uppercase font-medium text-secondary">Coming Soon</h1>
             </div>
         </>
