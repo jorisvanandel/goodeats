3981dc72f5f809076899da89d4497dc76213d3e7
andelwebsolutions <info@andelwebsolutions.nl>
2025-04-13 17:14:36 +0200

    chore: bump laravel/framework to v12.8.1

---
+++ app/Http/Resources/RestaurantResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property Restaurant $resource
+ */
+class RestaurantResource extends JsonResource
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->resource->id,
+            'name' => $this->resource->name,
+            'city' => $this->resource->city->value,
+            'address' => $this->resource->address,
+        ];
+    }
+}
---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property User $resource
+ */
+class UserResource extends JsonResource
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->resource->id,
+            'email' => $this->resource->email,
+            'username' => $this->resource->username,
+        ];
+    }
+}
