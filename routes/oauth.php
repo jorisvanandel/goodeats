ec398f61835b23b04d7eadd0d7cc13c0c5a55842
jorisvanandel <joris.andel@hotmail.com>
2025-04-25 17:01:17 +0200

    refactor: log google user on callback

--- routes/oauth.php
+++ routes/oauth.php
@@ -2,6 +2,7 @@
 
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
 
@@ -11,6 +12,8 @@ Route::get('/oauth/google/redirect', function () {
 
 Route::get('/oauth/google/callback', function () {
     $googleUser = Socialite::driver('google')->user();
+
+    Log::info($googleUser);
 
     $user = User::query()->updateOrCreate([
         'oauth_driver_id' => $googleUser->id,
